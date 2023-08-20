5993aaa749f087ec2b3aacc3616dcad6341fe8c7
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2023-08-20 23:39:12 +0530

    pattern print
    ****
    ***
    **
    *

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+//to print 
+/*
+   ****
+   ***
+   **
+   *
+ */
+class Pattern1{
+    public static void main(String[] args){
+        for(int j =4 ;j>0;j--){
+            for(int i = j ;i>0;i--){
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
