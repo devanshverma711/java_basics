6c342303b925d79d0466bfdb23fbd9b9aa809ab6
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2023-08-22 07:13:59 +0530

    Pattern

---
+++ Pattern3.java
@@ -0,0 +1,12 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        char ch ='A';
+        for(int i =1;i<=4;i++){
+            for(char j =1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
