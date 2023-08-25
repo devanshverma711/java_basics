925abc36918b6119d3f7052c859b05e09121b03d
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2023-08-25 21:07:43 +0530

    Pairs in array

---
+++ PairA.java
@@ -0,0 +1,14 @@
+public class PairA {
+    public static void main(String[] args) {
+        int number[]= {2,4,6,8,10};
+        int tp=0;
+        for(int i =0;i<number.length;i++){
+            for(int j=i+1;j<number.length;j++){
+                System.out.print("("+number[i]+","+ number[j] +") ");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs -"+tp);
+    }
+}
