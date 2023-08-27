94895db3010f2e90d88baa6898f25a999f73a492
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2023-08-27 16:34:40 +0530

    Diagonal sum of Matrix

---
+++ DiagonalMatrix.java
@@ -0,0 +1,35 @@
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}};
+
+        Diagonal(matrix);
+    }
+
+    private static void Diagonal(int[][] matrix) {
+        int sum =0;
+       /* 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(i==j){
+                    sum+=matrix[i][j];
+                }
+                else if(i+j==matrix.length-1){
+                    sum+=matrix[i][j];
+                }
+            }
+        }
+        */
+        for(int i=0;i<matrix.length;i++){
+            //pd
+            sum+=matrix[i][i];
+            //sd
+            if(i!=matrix.length)
+            sum+=matrix[i][matrix.length-1-i];
+        }
+        System.out.println(sum);
+    }
+    
+}
