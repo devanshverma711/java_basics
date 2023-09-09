27f64ef7dc382da54c01dfb6fb3d34ecdf9f21e4
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2023-09-09 23:44:32 +0530

    Oops in jaav

---
+++ Human.java
@@ -0,0 +1,15 @@
+public class Human {
+    int age;
+    String name;
+    int salary;
+    boolean married;
+    static int population;
+
+    public Human(int age, String name, int salary, boolean married) {
+        this.age = age;
+        this.name = name;
+        this.salary = salary;
+        this.married = married;
+        population+=1;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Human Devansh = new Human(22,"Devansh",00,false);
+        Human Rahul = new Human(22,"Rahul",10000,false);
+        System.out.println(Devansh.population);
+    }
+}
